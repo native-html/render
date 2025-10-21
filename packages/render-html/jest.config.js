c8854a3fc66726553c1ab7091ddf0ddd3e33ad26
Artur Bilski <admin@arturbilski.pl>
2025-10-21 08:15:42 +0200

    chore: fix tests

--- packages/render-html/jest.config.js
+++ packages/render-html/jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  moduleNameMapper: {
+    '@native-html/css-processor': '<rootDir>/../css-processor/src',
+    '@native-html/transient-render-engine':
+      '<rootDir>/../transient-render-engine/src'
+  },
   testRegex: 'src/.*\\.test\\.tsx?$',
   coveragePathIgnorePatterns: ['/node_modules/', '__tests__'],
   transformIgnorePatterns: [
