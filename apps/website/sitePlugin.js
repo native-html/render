8d91159e7e675ed889a00b149222e176059bb374
Michal Palczynski <michal.palczynski@ext.chime.com>
2025-12-08 13:53:43 +0100

    Fix Webpack 5 issues by adding polyfill

---
+++ apps/website/sitePlugin.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+  return {
+    name: 'custom-docusaurus-plugin',
+    // eslint-disable-next-line
+    configureWebpack(config, isServer, utils) {
+      return {
+        resolve: {
+          alias: {
+            path: require.resolve('path-browserify')
+          },
+          fallback: {
+            fs: false,
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify/browser')
+          }
+        }
+      };
+    }
+  };
+}
