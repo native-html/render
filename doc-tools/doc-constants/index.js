00a1f830af6328bf7bc9f849dae2bdc5c5e35982
5ZYSZ3K <pawel.poreba@swmansion.com>
2026-02-20 14:11:13 +0100

    fix: adjust website root

--- doc-tools/doc-constants/index.js
+++ doc-tools/doc-constants/index.js
@@ -1,5 +1,5 @@
-const WEBSITE_ROOT = 'https://meliorence.github.io';
-const WEBSITE_BASE = '/';
+const WEBSITE_ROOT = 'https://native-html.github.io';
+const WEBSITE_BASE = '/render/';
 const WEBSITE_URL = WEBSITE_ROOT + WEBSITE_BASE;
 
 module.exports = { WEBSITE_ROOT, WEBSITE_BASE, WEBSITE_URL };
